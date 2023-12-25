cd86e8265004d991b2e71ebc10181db368bfbef8
nikolalx <nikilx123@gmail.com>
2023-12-25 23:39:05 +0100

    .

---
+++ rekapitulacija.js
@@ -0,0 +1,14 @@
+// if(x%2 == 0)
+// c = ‘E’;
+// else
+// c = ‘O’;    
+
+// let x = 9
+
+// let c = (x%2 ==0) ? 'E' : 'O'
+
+// console.log(c);
+
+// String grade = (mark >= 90) ? “A” : (mark >= 80) ? “B” : “C”;
+
+String grade;
